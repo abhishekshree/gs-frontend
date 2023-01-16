5a829aac8ecfe415eeaac2314168a6d07c832207
Krishnansh5 <5272.krishnansh@gmail.com>
2023-01-16 22:10:26 +0530

    CSS changes + codebase cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import './App.css';
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
-import * as ttapi from '@tomtom-international/web-sdk-services'
-import * as tt from '@tomtom-international/web-sdk-maps';
-import { useRef,useState,useEffect } from 'react';
+import { useState,useEffect } from 'react';
 import Map from "./Map.js"
 import { destinations } from './constants';
+import SwipeableEdgeDrawer from './components/SwipeableEdgeDrawer';
 
 function App() {
-
+  const [scrnDim,setScrnDim] = useState({w:0,h:0});
   const [currLocation,setCurrLocatoin] = useState({lat: 12.9140182, lng: 77.5747463});
   const [deliveryLocation,setDeliveryLocation] = useState({lat: 12.9414398, lng: 77.5454111});
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScrnDim({w: window.screen.width, h: window.screen.height});
+    }
     // setCurrLocation // use set timeout to timely update the drivers geolocation
-  })
+  },[])
 
   return (
     <div className="App">
-      GROW SIMPLEE
-      <Map currLocation= {currLocation} deliveryLocation= {deliveryLocation} destinations={destinations} zoom_level={12} travel_mode="truck" />
+      {/* <div style={{width:scrnDim.w,height:scrnDim.h}}> */}
+        <SwipeableEdgeDrawer />
+        <Map currLocation= {currLocation} deliveryLocation= {deliveryLocation} destinations={destinations} zoom_level={12} travel_mode="truck" />
+      {/* </div> */}
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -3,12 +3,9 @@ import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as ttservices from "@tomtom-international/web-sdk-services";
 import tt from "@tomtom-international/web-sdk-maps";
 import { useEffect, useState, useRef } from "react";
-// import { fromSeconds } from "from-seconds";
 import { api_key } from "./constants.js"
-import axios from "axios"
 
 function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_mode }) {
-
     const mapElement = useRef();
     const [map, setMap] = useState(null); 
     const [waypoints,setWayPoints] = useState(destinations);
@@ -23,102 +20,6 @@ function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_
 
         return marker;
     }
-    let URL = `https://api.tomtom.com/routing/waypointoptimization/1?key=${api_key}`;
-    // const optimize_routes = () => {
-    //     const data = {
-    //         waypoints: waypoints.map((location) => {
-    //             return {
-    //             point: {
-    //                 latitude: location.lat,
-    //                 longitude: location.lng,
-    //             },
-    //             };
-    //         }),
-    //         options: {
-    //             travelMode: travel_mode,
-    //             vehicleMaxSpeed: 0,
-    //             vehicleCommercial: true,
-    //             vehicleLoadType: ["otherHazmatGeneral"],
-    //             traffic: "live",
-    //             departAt: "now",
-    //             outputExtensions: ["travelTimes", "routeLengths"],
-    //             waypointConstraints: {
-    //             originIndex: 0,
-    //             destinationIndex: 0,
-    //             },
-    //         },
-    //     };
-    //     fetch(URL, {
-    //     method: "POST",
-    //     headers: { "Content-type": "application/json;charset=UTF-8" },
-    //     body: JSON.stringify(data),
-    //     })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         const optimized_locations = data.summary.legSummaries.map((summary) => {
-    //             return { ...waypoints[summary.originIndex], ...summary };
-    //         });
-    //         console.log(optimized_locations);
-    //         optimized_locations.forEach((location, index) => {
-    //             const start_time = new Date(location.departureTime).toLocaleString();
-    //             const arrival_time = new Date(location.arrivalTime).toLocaleString();
-    //             const distance_in_seconds = location.travelTimeInSeconds
-    //             const start_at = location.originIndex;
-    //             const end_at = location.destinationIndex;
-    //             const popup = new tt.Popup({ offset: 50 }).setHTML(
-    //               `<div class="popup">
-    //               <h1>Location ${index}</h1>
-    //               <br />
-    //               <p>Current Point: ${waypoints[start_at].name}</p>
-    //               <p>Departure Time: ${start_time}</p>
-    //               <p>Next Stop: ${waypoints[end_at].name}</p>
-    //               <p>Arrival Time:  ${arrival_time}<p>
-    //               <p>Distance To next stop:  ${location.lengthInMeters / 1000}(km)</p>
-    //               <p>Estimated Time To next stop:  ${
-    //                 distance_in_seconds.hours
-    //               } Hours, ${distance_in_seconds.minutes} Minutes</p>
-    //               </div> `
-    //             );
-    //             create_delivery_marker(location).setPopup(popup);
-    //         })
-    //         create_route(optimized_locations);
-    //     })
-    // };
-
-    // const create_route = async (locations) => {
-    //     await ttservices.services
-    //         .calculateRoute({
-    //         key: api_key,
-    //         locations,
-    //         })
-    //         .then((routeData) => {
-    //         const features = routeData.toGeoJson().features;
-    //         features.forEach((feature, index) => {
-    //             map.addLayer({
-    //             id: "route" + index,
-    //             type: "line",
-    //             source: {
-    //                 type: "geojson",
-    //                 data: feature,
-    //             },
-    //             paint: {
-    //                 "line-color": `red`,
-    //                 "line-opacity": 0.8,
-    //                 "line-width": 6,
-    //                 "line-dasharray": [1, 0, 1, 0],
-    //             }
-    //             });
-    //         });
-    //         });
-    // };
-
-    // function createAllRoutes(){
-    //     let pts_covered = 0;
-    //         while(pts_covered<waypoints.length){
-    //             create_route(waypoints.slice(pts_covered,Math.min(pts_covered+150,waypoints.length)))
-    //             pts_covered+=150    
-    //         }       
-    // }
 
     function createRoute () {
         console.log(currLocation)
@@ -128,9 +29,9 @@ function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_
             [currLocation.lng, currLocation.lat],
             [deliveryLocation.lng, deliveryLocation.lat],
           ],
-          // travelMode: travel_mode,
-          // vehicleCommercial: true,
-          // vehicleHeading: 0,
+          travelMode: travel_mode,
+          vehicleCommercial: true,
+          vehicleHeading: 0,
         };
         
         ttservices.services.calculateRoute(routeOptions).then((response) => {
@@ -165,62 +66,6 @@ function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_
         });
     }
 
-    const getSnapFunction = () => {
-      // const url = 'https://api.tomtom.com/snapToRoads/1';
-      // const params = {
-      //   points: `${currLocation.lng},${currLocation.lat};${deliveryLocation.lng},${deliveryLocation.lat}`,
-      //   vehicleType: 'Truck',
-      //   fields: '{projectedPoints{type,geometry{type,coordinates}'+
-      //           ',properties{routeIndex,snapResult}}'+
-      //           ',route{type,geometry{type,coordinates}'+
-      //           ',properties{id,linearReference,speedLimits{value,unit,type}'+
-      //           ',speedProfile{value,unit},address{roadName,roadNumbers}}'+
-      //           ',frc,formOfWay,roadUse,laneInfo{numberOfLanes}'+
-      //           ',heightInfo{height,chainage},trafficSigns{signType,chainage}'+
-      //           ',trafficLight,confidence}}'+
-      //           ',distances{total,road,offRoad}}',
-      //   key: api_key,
-      // };
-      axios.get("https://api.tomtom.com/snap-to-roads/1/snap-to-roads?points=77.5747463,12.9140182;77.5454111,12.9414398&fields={projectedPoints{type,geometry{type,coordinates},properties{routeIndex}},route{type,geometry{type,coordinates},properties{id,speedRestrictions{maximumSpeed{value,unit}}}}}&key=y1Duis78asfADSQn9HYekaSyUw4siXLf").then((res) => 
-      {
-        console.log("done")
-       console.log(res.data)
-       res.data.route.forEach(
-        (item) => {
-          map.addLayer({
-            id: Math.random().toString(),
-            type: "line",
-            source: {
-              type: "geojson",
-              data: {
-                type: "FeatureCollection",
-                features: [
-                  {
-                    type: "Feature",
-                    geometry: {
-                      type: "LineString",
-                      properties: {},
-                      coordinates: item.geometry.coordinates
-                    }
-                  }
-                ]
-              }
-            },
-            layout: {
-              "line-cap": "round",
-              "line-join": "round"
-            },
-            paint: {
-              "line-color": "#ff0000",
-              "line-width": 2
-            }
-          });
-        }
-       )
-         map.setCenter([parseFloat(77.5747463), parseFloat(12.9140182)]);
-     }).catch((err) => console.log(err))
-    }
-
     useEffect(() => {
         let map = tt.map({
         key: api_key,
@@ -237,13 +82,9 @@ function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_
     useEffect(() => {
         if (map) {
           map.on("load", () => {
-            // for (let d in waypoints) {
-            //     create_delivery_marker(waypoints[d],map);
-            // }
             createRoute()
             create_delivery_marker(currLocation,map)
             create_delivery_marker(deliveryLocation,map)
-            // getSnapFunction()
           });
         }
         else{
@@ -252,9 +93,20 @@ function Map({ currLocation, deliveryLocation, destinations, zoom_level, travel_
       }, [map]);
 
     return (
-      <div className="map_wrapper">
-        <div ref={mapElement} className="mapDiv" />
-      </div>
+      // <div className="map_wrapper">
+        <div 
+          ref={mapElement} 
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            width: '100%',
+            height: '100%'
+          }}
+        />
+      // </div>
     );
   }
   export default Map;
